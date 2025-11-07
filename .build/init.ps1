5590e503f6258e9b0e489da274a3332389f70d0e
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2025-11-07 00:21:08 -0500

    clean up file and manifest setup

--- .build/init.ps1
+++ .build/init.ps1
@@ -3,25 +3,25 @@ $packDir = Split-Path -Parent $scriptDir
 $pack_name = Split-Path -Leaf $packDir
 $mojang = "$home/AppData/Roaming/Minecraft Bedrock/Users/Shared/games/com.mojang"
 
-$dev = $true
-
-echo $pack
-
 #symlinks
-# Remove-Item -r -Path "$mojang/development_behavior_packs/$pack_name" 
-# New-Item -ItemType SymbolicLink `
-#     -Path "$mojang/development_behavior_packs/$pack_name" `
-#     -Target "$packDir/BP"
-#
-# Remove-Item -r -Path "$mojang/development_resource_packs/$pack_name"
-# New-Item -ItemType SymbolicLink `
-#     -Path "$mojang/development_resource_packs/$pack_name" `
-#     -Target "$packDir/RP"
+Remove-Item -r -Path "$mojang/development_behavior_packs/$pack_name" 
+New-Item -ItemType SymbolicLink `
+    -Path "$mojang/development_behavior_packs/$pack_name" `
+    -Target "$packDir/BP"
 
-#uuid
+Remove-Item -r -Path "$mojang/development_resource_packs/$pack_name"
+New-Item -ItemType SymbolicLink `
+    -Path "$mojang/development_resource_packs/$pack_name" `
+    -Target "$packDir/RP"
 
-echo "$packDir/BP/manifest.json" 
+#manifest.json
 
-Get-Content "$packDir/BP/manifest.json" `
-| ForEach-Object { $_ -replace "`$uuid", [guid]::NewGuid() } `
-| Set-Content "$packDir/BP/manifest.json"
+(Get-Content "$packDir/BP/manifest.json") `
+ -replace '\$uuid', {[guid]::NewGuid().ToString()} `
+ -replace '\$pack_name', $pack_name `
+| Set-Content "$packDir/BP/manifest.json" 
+
+(Get-Content "$packDir/RP/manifest.json") `
+ -replace '\$uuid', {[guid]::NewGuid().ToString()} `
+ -replace '\$pack_name', $pack_name `
+| Set-Content "$packDir/RP/manifest.json" 
